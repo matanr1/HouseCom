1d8cda00f909a5bba8099e06a6a513a75d8d6a79
Davideinise <david.eini.se@gmail.com>
2014-07-19 06:36:13 +0300

    Map Api

---
+++ HouseCom/Scripts/Maps/mapClass.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMxXXx_oaTw-wxmFL5I-W8jR-Y3eJYtbA&language=he">
+
+
+google.maps.Map.prototype.markers = new Array();
+
+google.maps.Map.prototype.getMarkers = function () {
+    return this.markers
+};
+
+google.maps.Map.prototype.clearMarkers = function () {
+    for (var i = 0; i < this.markers.length; i++) {
+        this.markers[i].setMap(null);
+    }
+    this.markers = new Array();
+};
+
+google.maps.Marker.prototype._setMap = google.maps.Marker.prototype.setMap;
+
+google.maps.Marker.prototype.setMap = function (map) {
+    if (map) {
+        map.markers[map.markers.length] = this;
+    }
+    this._setMap(map);
+}
