58a3fbe990072be4fc0dbad071a9f60e79984f82
Davideinise <david.eini.se@gmail.com>
2014-07-24 11:29:07 +0300

    Employee Customer

---
+++ HouseCom/Classes/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HouseCom.Classes
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public Employee CreatedBy { get; set; }
+        public Employee ModifiedBy { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
