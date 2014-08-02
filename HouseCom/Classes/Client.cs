2ded7563c441d12ea3482e0f783b319199473f57
matanr1 <matanr1@gmail.com>
2014-08-02 06:40:13 +0300

    add client and property

---
+++ HouseCom/Classes/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HouseCom.Classes
+{
+    public class Client
+    {
+        public int id { get; set; }
+
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string mail { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        
+    }
+}
---
+++ HouseCom/Classes/Property.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HouseCom.Classes
+{
+    public class Property
+    {
+        public int pId {get;set;}
+        public string type {get;set;}
+        public long rooms { get; set; }
+        public string city { get; set; }
+        public bool state { get; set; }
+        public bool ForSell { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int price { get; set; }
+    }
+}
