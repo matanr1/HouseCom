c352193dbfa3d1aecfae550da159681eed38cca3
Davideinise <david.eini.se@gmail.com>
2014-08-03 15:18:48 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	HouseCom.v12.suo

---
+++ HouseCom/Classes/Property.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HouseCom.Classes
+{
+    public class Property
+    {
+        public int pId {get;set;}
+        public string type {get;set;}
+        public long rooms { get; set; }
+        public string city { get; set; }
+        public bool state { get; set; }
+        public bool ForSell { get; set; }
+        public bool forRent { get; set; }
+        public Marker mark {get; set;}
+        public DateTime DateCreated { get; set; }
+        public int price { get; set; }
+    }
+}
